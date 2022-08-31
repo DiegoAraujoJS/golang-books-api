d5b73431aafe8c933055476c9d3a5f7a57e2db20
DiegoAraujoJS <diegolaraujo96@gmail.com>
2022-08-31 07:48:55 -0300

    changes to controller, move code to routes

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,14 +1,52 @@
-package routes
+package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/DiegoAraujoJS/golang-books-api/pkg/controllers"
+	"net/http"
+	"strconv"
+	"github.com/DiegoAraujoJS/golang-books-api/pkg/utils"
+	"github.com/DiegoAraujoJS/golang-books-api/pkg/models"
 )
 
-var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+var NewBook models.Book
+
+// type Book struct {
+//	gorm.model
+//	Name string `gorm:""json:"name"`
+//	Author string `json:"author"`
+//	Publication string `json:"publication"`
+//}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOk)
+	w.Write(res)
+}
+
+
+func GetBookById(w http.ResponseWriter, r *http.Request){
+	var book Book
+	allBooks = models.GetAllBooks()
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error parsing")
+	}
+	book, _ = models.GetModelById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request){
+	var newBook Book
+	vars := mux.Vars(r)
+	newBook
+	createdBook := models.CreateBook(&NewBook)
 }
